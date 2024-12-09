7ebc10c054b2d77d02a8eb1f3bc402a672185ddb
Leandro Molina <leolean005@gmail.com>
2024-12-09 20:46:02 -0300

    Panel admin listo

--- gestor_taller/admin.py
+++ gestor_taller/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Mecanico, OrdenTrabajo, Tarea
 
 # Register your models here.
+
+# Registrar los modelos en el panel de administración
+admin.site.register(Mecanico)
+admin.site.register(OrdenTrabajo)
+admin.site.register(Tarea)
