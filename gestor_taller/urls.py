57905b8a3335c1323482c9cd1fba374e905c0cb2
Leandro Molina <leolean005@gmail.com>
2024-12-10 16:16:50 -0300

    Mejorando la interfaz

--- gestor_taller/urls.py
+++ gestor_taller/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('ordenes/<int:orden_id>/', views.detalle_orden, name='detalle_orden'),  # Detalle de una orden
     path('mecanicos/', views.lista_mecanicos, name='lista_mecanicos'),  # Lista de mecánicos
     path('tareas/', views.lista_tareas, name='lista_tareas'),  # Lista de tareas
+    path('ordenes/buscar/', views.buscar_orden, name='buscar_orden'),  # Nueva ruta para buscar órdenes
 ]
