9c36385a4d95c344bf9bcddd1b3a558b23411fe5
Leandro Molina <leolean005@gmail.com>
2024-12-09 21:51:55 -0300

    Agregue varias vistas

--- gestor_taller/urls.py
+++ gestor_taller/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # Ruta para listar las órdenes de trabajo
-    path('ordenes/', views.lista_ordenes, name='lista_ordenes'),
+    path('ordenes/', views.lista_ordenes, name='lista_ordenes'),  # Lista de órdenes
+    path('ordenes/<int:orden_id>/', views.detalle_orden, name='detalle_orden'),  # Detalle de una orden
+    path('mecanicos/', views.lista_mecanicos, name='lista_mecanicos'),  # Lista de mecánicos
+    path('tareas/', views.lista_tareas, name='lista_tareas'),  # Lista de tareas
 ]
