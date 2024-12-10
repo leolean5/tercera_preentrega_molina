de09300c738dcedfc1e96d033c48b1fb4ad83ee8
Leandro Molina <leolean005@gmail.com>
2024-12-09 21:36:13 -0300

    Vista de lista de ordenes funcionando

--- gestor_taller/views.py
+++ gestor_taller/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import OrdenTrabajo
 
 # Create your views here.
+
+# Vista para listar todas las órdenes de trabajo
+def lista_ordenes(request):
+    # Consultar todas las órdenes en la base de datos
+    ordenes = OrdenTrabajo.objects.all()
+    # Pasar las órdenes al template como contexto
+    return render(request, 'lista_ordenes.html', {'ordenes': ordenes})
+
+
